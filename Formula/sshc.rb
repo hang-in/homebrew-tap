8db893aec97ece396ca2807c489b4c185e9581ac
hang-in <kurthong88@gmail.com>
2026-05-20 13:30:39 +0900

    sshc 0.4.2 (cargo-dist generated)

--- Formula/sshc.rb
+++ Formula/sshc.rb
@@ -1,44 +1,73 @@
-# typed: false
-# frozen_string_literal: true
-
 class Sshc < Formula
   desc "Minimal TUI for managing and connecting to SSH hosts defined in ~/.ssh/config"
   homepage "https://github.com/hang-in/sshc"
-  version "0.4.1"
-  license "MIT"
-
-  on_macos do
-    on_arm do
-      url "https://github.com/hang-in/sshc/releases/download/v0.4.1/sshc-aarch64-apple-darwin.tar.gz"
-      sha256 "e561b5ba7436a04cf503ea4a1eb19ef71e91318b440eaa56c2d06eb2912ad2e0"
+  version "0.4.2"
+  if OS.mac?
+    if Hardware::CPU.arm?
+      url "https://github.com/hang-in/sshc/releases/download/v0.4.2/sshc-aarch64-apple-darwin.tar.xz"
+      sha256 "9e333b2bc0731c57ef385b4b27473f4d4ee429485d37fee6e00f3d076dbd471e"
     end
-    on_intel do
-      # macos-13 runner is still queued at release time. Update the URL +
-      # sha256 once the x86_64-apple-darwin tarball is attached.
-      url "https://github.com/hang-in/sshc/releases/download/v0.4.1/sshc-x86_64-apple-darwin.tar.gz"
-      sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+    if Hardware::CPU.intel?
+      url "https://github.com/hang-in/sshc/releases/download/v0.4.2/sshc-x86_64-apple-darwin.tar.xz"
+      sha256 "2cfc9f53b79d1364160ba10aa9cd252ece0bc06c3fdb7b5e924a398f986797a8"
     end
   end
-
-  on_linux do
-    on_arm do
-      url "https://github.com/hang-in/sshc/releases/download/v0.4.1/sshc-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "4b49dcb4215bca397023c1fda871f69bfe65f0fdbe6493bcdbb3354d849670a5"
+  if OS.linux?
+    if Hardware::CPU.arm?
+      url "https://github.com/hang-in/sshc/releases/download/v0.4.2/sshc-aarch64-unknown-linux-gnu.tar.xz"
+      sha256 "5dba54c5125acf63cc7de4ce608fb5ede5159d82a4e06d86e8d5d50ac2e0e258"
     end
-    on_intel do
-      url "https://github.com/hang-in/sshc/releases/download/v0.4.1/sshc-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "8533af199299c153414f75ef9e3114d6c6e2bc0b0e32fdc6acbf483dc35907ad"
+    if Hardware::CPU.intel?
+      url "https://github.com/hang-in/sshc/releases/download/v0.4.2/sshc-x86_64-unknown-linux-gnu.tar.xz"
+      sha256 "ce01563671d1db4c3d4d0a1bb4e11b521903cdb48c0b8033594d93e3833c6d2d"
+    end
+  end
+  license "MIT"
+
+  BINARY_ALIASES = {
+    "aarch64-apple-darwin": {},
+    "aarch64-unknown-linux-gnu": {},
+    "x86_64-apple-darwin": {},
+    "x86_64-unknown-linux-gnu": {}
+  }
+
+  def target_triple
+    cpu = Hardware::CPU.arm? ? "aarch64" : "x86_64"
+    os = OS.mac? ? "apple-darwin" : "unknown-linux-gnu"
+
+    "#{cpu}-#{os}"
+  end
+
+  def install_binary_aliases!
+    BINARY_ALIASES[target_triple.to_sym].each do |source, dests|
+      dests.each do |dest|
+        bin.install_symlink bin/source.to_s => dest
+      end
     end
   end
 
   def install
-    bin.install "sshc"
-  end
+    if OS.mac? && Hardware::CPU.arm?
+      bin.install "sshc"
+    end
+    if OS.mac? && Hardware::CPU.intel?
+      bin.install "sshc"
+    end
+    if OS.linux? && Hardware::CPU.arm?
+      bin.install "sshc"
+    end
+    if OS.linux? && Hardware::CPU.intel?
+      bin.install "sshc"
+    end
 
-  test do
-    # sshc is a TUI that requires a tty; running it headless would hang.
-    # Smoke-test that the binary was installed and is executable.
-    assert_predicate bin/"sshc", :exist?
-    assert_predicate bin/"sshc", :executable?
+    install_binary_aliases!
+
+    # Homebrew will automatically install these, so we don't need to do that
+    doc_files = Dir["README.*", "readme.*", "LICENSE", "LICENSE.*", "CHANGELOG.*"]
+    leftover_contents = Dir["*"] - doc_files
+
+    # Install any leftover files in pkgshare; these are probably config or
+    # sample files.
+    pkgshare.install(*leftover_contents) unless leftover_contents.empty?
   end
 end
