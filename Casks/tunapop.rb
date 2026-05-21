929b2cc1861f8a964c8b489792ec1aacda53230b
d9ng <d9ng@outlook.com>
2026-05-22 06:19:39 +0900

    bump: tunapop to v0.2.1

--- Casks/tunapop.rb
+++ Casks/tunapop.rb
@@ -1,6 +1,6 @@
 cask "tunapop" do
-  version "0.1.0"
-  sha256 "c0ebd09b268eeaefe57fc49d5bc855cb8ba440edf9c15316e7885f5216ae43a0"
+  version "0.2.1"
+  sha256 "05d76a6d964a73089cce22dbf42887f0461d32050c1bbec3e82ddfafb445f70b"
 
   url "https://github.com/hang-in/tunapop/releases/download/v#{version}/tunaPop-#{version}.dmg"
   name "tunaPop"
