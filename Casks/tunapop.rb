4b93cb25545c423808abddd5c96be6b8c20b2927
d9ng <96920164+hang-in@users.noreply.github.com>
2026-05-22 05:09:59 +0900

    Add tunapop cask v0.1.0

---
+++ Casks/tunapop.rb
@@ -0,0 +1,17 @@
+cask "tunapop" do
+  version "0.1.0"
+  sha256 "6cbff6ed0aa141ebf95702ab277003048c78500754b71d8dc60f03e0e85c9010"
+
+  url "https://github.com/hang-in/tunapop/releases/download/v#{version}/tunaPop-#{version}.dmg"
+  name "tunaPop"
+  desc "PopClip-style macOS assistant powered by local LLMs"
+  homepage "https://github.com/hang-in/tunapop"
+
+  app "tunaPop.app"
+
+  zap trash: [
+    "~/Library/Application Support/tunaPop",
+    "~/Library/Logs/tunaPop",
+    "~/Library/Preferences/app.tunapop.plist"
+  ]
+end
